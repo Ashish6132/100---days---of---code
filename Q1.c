f01ee4b3f9c52db01927791345b7d1233feb14c8
Ashish6132 <ashishrai6132@gmail.com>
2025-11-01 20:40:30 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a , b;
+    printf ("enter the two number:");
+    scanf ("%d %d",&a,&b);
+    int sum = 0;
+    sum = a+b;
+    printf ("the sum is %d",a+b);
+    return 0;
+}
